572b86e95fa43eac85dfc69274e90e946785977c
pro100imba-spec <pro100imba@gmail.com>
2025-09-02 09:41:16 +0300

    modified:   lib/design/colors.dart

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -4,4 +4,21 @@ import 'package:flutter/material.dart';
 const Color mainGreenColor = Color(0xFF2ECC70);
 const Color mainBlueColor = Color(0xFF379AE6);
 
-const Color backgroundColor = Color(0xFFF3F4F6);
+const Color backgroundGreyColor = Color(0xFFF3F4F6);
+
+const Color redColor = Color(0xFFFF0008);
+const Color backgroundRedColor = Color(0xFFFDF2F2);
+
+const Color yellowColor = Color(0xFFEB9D00);
+const Color backgroundYellowColor = Color(0xFFFFF6E2);
+
+const Color greenColor = Color(0xFF17A948);
+const Color backgroundGreenColor = Color(0xFFEEFDF3);
+
+const Color goldColor = Color(0xFFECA517);
+const Color blackColor = Color(0xFF323743);
+
+const Color ownChatColor = Color(0xFFDBFFDC);
+
+const Color manNameColor = Color(0xFF197DCA);
+const Color womanNameColor = Color(0xFFE02862);
