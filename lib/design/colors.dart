6d25c05f70dec77f55d2f424c24836d7f2682ca2
pro100imba-spec <pro100imba@gmail.com>
2025-09-02 09:10:52 +0300

    new file:   lib/design/colors.dart

---
+++ lib/design/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const Color mainGreenColor = Color(0xFF2ECC70);
+const Color mainBlueColor = Color(0xFF379AE6);
+
+const Color backgroundColor = Color(0xFFF3F4F6);
