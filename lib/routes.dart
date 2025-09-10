bffc7f84a0d7f13e70b917d8dfc0a3d326166067
Алексей Фомин <>
2025-09-10 09:02:06 +0300

    modified:   lib/routes.dart 	modified:   lib/screens/home_screen.dart 	new file:   linux/flutter/ephemeral/.plugin_symlinks/flutter_secure_storage_linux 	new file:   linux/flutter/ephemeral/.plugin_symlinks/path_provider_linux 	new file:   linux/flutter/generated_plugin_registrant.cc 	new file:   linux/flutter/generated_plugin_registrant.h 	new file:   linux/flutter/generated_plugins.cmake 	new file:   windows/flutter/ephemeral/.plugin_symlinks/flutter_secure_storage_windows 	new file:   windows/flutter/ephemeral/.plugin_symlinks/path_provider_windows

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:paceip/screens/createacc_screen.dart';
+import 'screens/createacc_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/lenta_screen.dart';
