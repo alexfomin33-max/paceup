a2a92157e23ba7af5083a77edeb8d18c353173b1
pro100imba-spec <pro100imba@gmail.com>
2025-12-12 09:36:15 +0300

    правильная загрузка фото в клубах

--- lib/features/leaderboard/leaderboard_screen.dart
+++ lib/features/leaderboard/leaderboard_screen.dart
@@ -55,7 +55,7 @@ class _LeaderboardScreenState extends ConsumerState<LeaderboardScreen>
       // ── Глобальная шапка без нижнего бордера
       appBar: const PaceAppBar(
         title: 'Лидерборд',
-        showBack: true,
+        showBack: false,
         showBottomDivider: false,
       ),
 
